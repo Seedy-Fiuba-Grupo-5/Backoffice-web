a5e2f1de3585192a804e5501712bf70290d6b217
diegobalestieri <diego.balestieri1998@gmail.com>
2021-05-02 14:17:45 -0300

    agrego tabla y crear proyectos

--- projects-web/src/App/Projects.js
+++ projects-web/src/App/Projects.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import {Table, Form, Button} from "react-bootstrap";
 
 class Routes extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            projects: []
+            projects: [],
+            newProjectName: ""
         };
     }
 
@@ -22,14 +23,58 @@ class Routes extends Component {
         this.getProjects();
     }
 
+    postProject = event =>{
+        event.preventDefault();
+        const newProject = {
+            'name': this.state.newProjectName
+        }
+        axios.post('http://localhost:5000/projects', newProject);
+        this.getProjects();
+    }
+
+    updateStates = event => {
+        this.setState({
+            [event.target.name]:event.target.value
+        });
+    }
+
     render() {
         return (
             <div>
                 Proyectos:
-                {this.state.projects}
-                End.
+                <Table>
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Project Name</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.projects.map((project, index) => {
+                            return (
+                                <tr>
+                                    <td>{index}</td>
+                                    <td>{project}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </Table>
+                <Form onSubmit={this.postProject}>
+                    <Form.Group controlId="formBasicProject">
+                        <Form.Label>Project Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            name="newProjectName"
+                            onChange={this.updateStates}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
             </div>
-        )
+        );
     }
 }
 
