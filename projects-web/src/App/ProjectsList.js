8e1725abea20abf4738861b1311a63eb84ebb8a0
diegobalestieri <diego.balestieri1998@gmail.com>
2021-05-17 17:53:47 -0300

    ajusto allgunas cosas

--- projects-web/src/App/ProjectsList.js
+++ projects-web/src/App/ProjectsList.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import {Table, Form, Button} from "react-bootstrap";
-import { Link } from 'react-router-dom';
+import '../CSS/Grid.css';
 const URL = 'http://localhost:5000/'
 
 class ProjectsList extends Component {
@@ -16,7 +15,7 @@ class ProjectsList extends Component {
     getProjects(){
         axios.get(URL+'projects')
             .then(response => {
-                if(response.status === 201){
+                if(response.status === 200){
                     this.setState({projects : response.data})
                 }
             }).catch((err) => {
@@ -30,31 +29,12 @@ class ProjectsList extends Component {
         this.getProjects();
     }
 
-    postProject = event =>{
-        event.preventDefault();
-        const newProject = {
-            'name': this.state.newProjectName
-        }
-        axios.post(URL+'projects', newProject)
-            .then(response => {
-                if(response.status === 201){
-                    this.getProjects();
-                }
-            });
-    }
-
-    updateStates = event => {
-        this.setState({
-            [event.target.name]:event.target.value
-        });
-    }
-
     render() {
         return (
             <div className="row container">
                 {this.state.projects.map((project) => {
                     return (
-                        <div className="col s4">
+                        <div className="col-sm-4 s4">
                             <div className="row">
                                 <div className="col s12 m6">
                                     <div className="card blue-grey darken-1">
@@ -62,6 +42,8 @@ class ProjectsList extends Component {
                                             <span className="card-title">{project.name}</span>
                                             <p>I am a very simple card. I am good at containing small bits of information.
                                                 I am convenient because I require little markup to use effectively.</p>
+                                            <a className="btn-floating btn-large waves-effect waves-light blue" style={{float: "right"}} href={'/projects/'+project.id}><i
+                                                className="material-icons">add</i></a>
                                         </div>
                                     </div>
                                 </div>
@@ -75,45 +57,3 @@ class ProjectsList extends Component {
 }
 
 export default ProjectsList;
-
-/*
-<div className="container">
-                Proyectos:
-                <Table>
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Project Name</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.projects.map((project) => {
-                            return (
-                                <tr>
-                                    <td>{project.id}</td>
-                                    <td>{project.name}</td>
-                                    <td>
-                                        <Button>
-                                            <Link to={"/projects/" + project.id} style = {{color:'white'}}>Open</Link>
-                                        </Button>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </Table>
-                <Form onSubmit={this.postProject}>
-                    <Form.Group controlId="formBasicProject">
-                        <Form.Label>Project Name</Form.Label>
-                        <Form.Control
-                            type="text"
-                            name="newProjectName"
-                            onChange={this.updateStates}
-                        />
-                    </Form.Group>
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-            </div>
- */
