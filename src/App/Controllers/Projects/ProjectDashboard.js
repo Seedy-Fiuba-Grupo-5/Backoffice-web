80bfb826bfe870ab2b1147848f1c2e3acf31604f
diegobalestieri <diego.balestieri1998@gmail.com>
2021-07-02 15:30:39 -0300

    refactorizo grafico, no anda

--- src/App/Controllers/Projects/ProjectDashboard.js
+++ src/App/Controllers/Projects/ProjectDashboard.js
@@ -1,19 +1,16 @@
 import React, { Component } from 'react';
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import ApiController from "../ApiController";
 import {Messagebar} from "../../Components/Messagebar";
 import {getSetting} from "../../settings";
+import {ProjectsByType} from "../../Components/Graphs/ProjectsByType";
 const URL = getSetting('BACKEND_URL')+'/projects';
-
-const types = ["Art","Comics","Crafts","Dance","Design","Fashion","Film & Video","Food",
-    "Games","Journalism","Music","Photography","Publishing","Technology","Theater","Other"];
 
 export default class ProjectDashboard extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            report: [],
+            projects: [],
             showSnackbar: false,
             error: ''
         };
@@ -32,15 +29,7 @@ export default class ProjectDashboard extends Component {
 
     responseHandler(response) {
         if(response.status === 200){
-            let data = [];
-            const iterations = types.length;
-            for(let i = 0; i < iterations; i++){
-                let len = response.data.filter(function(item){
-                    return item.type === types[i];
-                }).length;
-                data.push({type: types[i], count: len});
-            }
-            this.setState({report : data});
+            this.setState({projects : response.data});
             this.setState({showSnackbar : true});
             this.setState({loading : false});
             this.setState({error: ''});
@@ -60,27 +49,8 @@ export default class ProjectDashboard extends Component {
     render() {
         return (
             <div className="container" style={{width: "100%", height:"100%"}}>
-                <h5>Amount of Projects by type</h5>
-                <ResponsiveContainer width="100%" height="50%">
-                    <BarChart
-                        width={500}
-                        height={300}
-                        data={this.state.report}
-                        margin={{
-                            top: 5,
-                            right: 30,
-                            left: 20,
-                            bottom: 5,
-                        }}
-                    >
-                        <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="type" />
-                        <YAxis />
-                        <Tooltip />
-                        <Legend />
-                        <Bar dataKey='count' fill="#8884d8" />
-                    </BarChart>
-                </ResponsiveContainer>
+                <h5>Amount of Projects by type </h5>
+                <ProjectsByType projects={this.state.projects}/>
                 {this.state.showSnackbar ?
                     <Messagebar
                         message={this.state.error.length > 0 ? this.state.error : "Projects loaded successfully"}
